b2216528c8fb0d2af48634f09dc4a150099d4315
aiprasad <49321587+aiprasad@users.noreply.github.com>
2023-05-25 22:43:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,19 @@
 import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from fastapi import FastAPI
 
-# Pretrained model name
-PRETRAINED_MODEL_NAME = "gpt2"
-
-# Initialize tokenizer
-tokenizer = GPT2Tokenizer.from_pretrained(PRETRAINED_MODEL_NAME)
-
-# Initialize model
-model = GPT2LMHeadModel.from_pretrained(PRETRAINED_MODEL_NAME)
-
-# Load state dictionary from pickle file
-model.load_state_dict(torch.load("pytorch_model.bin"))
-
 app = FastAPI()
+tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-3b")
+model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-3b")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.post("/generate/")
-async def generate_text(input: InputText):
-    # Preprocess text input
-    input_ids = tokenizer.encode(input.text, return_tensors="pt")
-    
-    # Generate text
-    # You may want to adjust parameters here for text generation
-    output = model.generate(input_ids, max_length=100, num_return_sequences=1, temperature=0.7)
-
-    # Decode generated text
-    generated_text = tokenizer.decode(output[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
-    
-    return {"generated_text": generated_text}
+def generate_text(prompt: str):
+    input_ids = tokenizer.encode(prompt, return_tensors="pt")
+    output = model.generate(input_ids, max_length=100, num_return_sequences=1)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    return {"prompt": prompt, "generated_text": generated_text}
 
